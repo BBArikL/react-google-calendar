02174af1fc29c1648c477dea3b5e26038966dc94
Arik <BBArik@protonmail.com>
2024-10-10 15:10:37 -0400

    Add dist

---
+++ dist/types/event.d.ts
@@ -0,0 +1,22 @@
+import { CSSObject } from '@emotion/react';
+import { DateTime } from 'luxon';
+import { CSSProperties } from 'react';
+export interface EventProps {
+  name: string;
+  startTime: DateTime;
+  endTime: DateTime;
+  description?: string;
+  location?: string;
+  eventStyles?: CSSProperties | CSSObject;
+  eventCircleStyles?: CSSProperties | CSSObject;
+  eventTextStyles?: CSSProperties | CSSObject;
+  tooltipStyles?: CSSProperties | CSSObject;
+  calendarName?: string;
+  color?: string;
+}
+
+export interface EventState {
+  startTime: DateTime;
+  endTime: DateTime;
+  showTooltip: boolean;
+}
---
+++ dist/types/index.d.ts
@@ -0,0 +1,29 @@
+import { DateTime, Zone } from 'luxon';
+export interface CalendarProps {
+  apiKey: string;
+  calendars: {
+    calendarId: string;
+    color?: string;
+  }[];
+  language?: string;
+  styles?: object;
+  showArrow?: boolean;
+  showFooter?: boolean;
+}
+
+export interface CalendarState {
+  monthNames: string[];
+  days: string[];
+  today: DateTime;
+  current: DateTime;
+  calendars: {
+    calendarId: string;
+    color?: string;
+  }[];
+  events: any[];
+  singleEvents: any[];
+  userTimezone: Zone;
+  showArrow: boolean;
+  showFooter: boolean;
+  processedCalendars: string[];
+}
